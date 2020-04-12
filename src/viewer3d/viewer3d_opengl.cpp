5319e306debc19901b55f6fd6f6ca582c53669cd
avkudr <argon24@mail.ru>
2020-04-12 18:48:43 +0200

    viewer3d: fix for compatibility with opengl >= 3.0

--- src/viewer3d/viewer3d_opengl.cpp
+++ src/viewer3d/viewer3d_opengl.cpp
@@ -164,7 +164,8 @@ void Viewer3DOpenGL::init()
     glGenFramebuffers(1, &m_fbo);
     glBindFramebuffer(GL_FRAMEBUFFER, m_fbo);
     // Set "renderedTexture" as our colour attachement #0
-    glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, *tId, 0);
+    //glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, *tId, 0);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, *tId, 0);
 
     // Set the list of draw buffers.
     GLenum DrawBuffers[1] = {GL_COLOR_ATTACHMENT0};
